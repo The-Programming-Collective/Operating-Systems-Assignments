f788a771aca0bbb8e6fd1c5cb425df94f71371ad
MainUseless <86481662+MainUseless@users.noreply.github.com>
2023-01-16 21:14:25 +0200

    Final

--- 2/Main.java
+++ 2/Main.java
@@ -1,4 +1,3 @@
-import java.util.DuplicateFormatFlagsException;
 import java.util.LinkedList;
 import java.util.Scanner;
 
@@ -7,126 +6,41 @@ public class Main {
         private static Scanner scanner = new Scanner(System.in);
         private static int NOP,CST;
         public static void main(String[] args) {
+        Process a1= new Process("p1", 0, 4, 0, 3);
+        Process a2= new Process("p2", 1, 8, 6, 3);
+        Process a3= new Process("p3", 3, 2, 11, 3);
+        Process a4= new Process("p4", 10, 6, 4, 3);
+        Process a5= new Process("p5", 12, 5, 5, 3);
 
-        int choice;
-        boolean isOn = true;
+        // Process a1= new Process("p1", 0, 17, 4, 7);
+        // Process a2= new Process("p2", 2, 6, 7, 9);
+        // Process a3= new Process("p3", 5, 11, 3, 4);
+        // Process a4= new Process("p4", 15, 4, 6, 6);
 
-        while(isOn){
-            System.out.print("\n=============================\n");
-            while(true){
-                try{
-                    System.out.print("Enter number of processes: ");
-                    NOP = Integer.parseInt(scanner.nextLine());
-                    System.out.print("Enter context switch time: ");
-                    CST = Integer.parseInt(scanner.nextLine());
-                    if(NOP<0 || CST<0){throw new Exception();}
-                    break;
-                }catch(Exception e){System.out.print("Error, enter valid ints.\n");}
-            }
-            queue.clear();
-            addProcesses();
-    
-            choice = -1;
-            
-            System.out.print("\nCurrent context switch time: "+CST+"\nCurrent processes count: "+queue.size()+"\nSelect The Desired Process Scheduler:-\n");
-            System.out.print("1-Preemptive Shortest-Job First (SJF).\n2-Round Robin (RR).\n3-Preemptive Priority Scheduling.\n4-AG Scheduling.\n5-Change Context switch time.\n6-Add processes.\n7-Exit.\n:");
-            try{choice = Integer.parseInt(scanner.nextLine());}
-            catch(Exception e){}
-            System.out.print("\n=============================\n");
-            switch (choice){
-                case 1:
-                    Scheduler preemptiveShortest = new SJFscheduler();
-                    preemptiveShortest.startScheduler(queue,CST);
-                    break;
 
-                case 2:
-                    int quantumTime = -1;
+        // Process a1= new Process("p1", 0, 5, 5, 7);
+        // Process a2= new Process("p2", 0, 2, 4, 9);
+        // Process a3= new Process("p3", 5, 8, 5, 4);
+        // Process a4= new Process("p4", 10, 5, 1, 6);
+        // Process a5= new Process("p5", 3, 7, 0, 3);
 
-                    while(true){
-                        try{
-                            System.out.print("Enter quantum time: ");
-                            quantumTime = Integer.parseInt(scanner.nextLine());
-                            if(quantumTime<=0){throw new Exception();}
-                                break;
-                        }catch(Exception e){System.out.print("Error, enter valid int.\n");}
-                    }
+        LinkedList<Process> queue = new LinkedList<>();
+        queue.addLast(a1);
+        queue.addLast(a2);
+        queue.addLast(a3);
+        queue.addLast(a4);
+        queue.addLast(a5);
 
-                    for(int i=0 ; i<queue.size() ; i++){
-                        queue.get(i).setQuantum(quantumTime);
-                    }
+        Scheduler rr = new RRscheduler();
+        rr.startScheduler(new LinkedList<Process>(queue), 1);
 
-                    Scheduler roundRobin = new RRscheduler();
-                    roundRobin.startScheduler(queue,CST);
-                    break;
+        // Scheduler ps = new Priorityscheduler();
+        // ps.startScheduler(new LinkedList<Process>(queue), 0);
 
-                case 3:
-                    Scheduler preemptivePriority = new Priorityscheduler();
-                    preemptivePriority.startScheduler(queue,CST);
-                    break; 
+        // Scheduler SJF = new SJFscheduler();
+        // SJF.startScheduler(new LinkedList<Process>(queue), 0);
 
-                case 4:
-                    Scheduler agSchedule = new AGscheduler();
-                    agSchedule.startScheduler(queue,CST);
-                    break;
-
-                case 5:
-                    while(true){
-                        try{
-                            System.out.print("Enter context switch time: ");
-                            CST = Integer.parseInt(scanner.nextLine());
-                            if(CST<0){throw new Exception();}
-                            break;
-                        }catch(Exception e){System.out.print("Error, enter valid int.\n");}
-                    }
-                    break;
-
-                case 6:
-                    while(true){
-                        try{
-                            System.out.print("Enter number of processes: ");
-                            NOP = Integer.parseInt(scanner.nextLine());
-                            if(NOP<0){throw new Exception();}
-                            break;
-                        }catch(Exception e){System.out.print("Error, enter valid int.\n");}
-                    }
-                    addProcesses();
-                    break;
-
-                case 7:
-                    isOn=!isOn;
-                    break;
-
-                default:
-                    System.out.print("Invalid selection.\n");
-                    break;
-            }
-        }
-        scanner.close();
-    }
-
-    public static void addProcesses(){
-        for(int i=0 ; i<NOP ; i++){
-            try{ 
-                System.out.print("Enter process "+(i+1)+" name, arrival time, burst time, priority,  quantum: ");
-                String[] output = scanner.nextLine().split(" ");
-
-                if(output.length!=5){throw new Exception();}
-
-                String name = output[0];
-                int arrivalTime = Integer.parseInt(output[1]);
-                int burstTime = Integer.parseInt(output[2]);
-                int priority = Integer.parseInt(output[3]);
-                int quantum = Integer.parseInt(output[4]);
-
-                if(arrivalTime<0 || priority<0 || burstTime<=0 || quantum<=0){throw new Exception();}
-
-                Process p = new Process(name, arrivalTime, burstTime, priority, quantum);
-                if(queue.contains(p)){throw new DuplicateFormatFlagsException(name);}
-
-                queue.addLast(p);
-            }
-            catch(DuplicateFormatFlagsException e){System.out.print("Error, process "+e.getMessage()+" already exists.\n");i--;}
-            catch(Exception e){System.out.print("Error, this process is not accepted.\n");i--;}
-        };
+        // Scheduler AG = new AGscheduler();
+        // AG.startScheduler(new LinkedList<Process>(queue),0);
     }
 }
